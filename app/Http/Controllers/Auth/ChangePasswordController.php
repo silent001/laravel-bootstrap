48a7f5300ec0eb58ba71f2fe583ed794f16968b9
silent001 <martinnr@live.co.za>
2022-08-09 14:05:44 +0200

    Comment fix

--- app/Http/Controllers/Auth/ChangePasswordController.php
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -43,7 +43,7 @@ class ChangePasswordController extends Controller
     }
 
     /**
-     * Display the password update view for the given token.
+     * Display the password update view for the current user.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
