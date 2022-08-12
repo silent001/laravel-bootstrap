aa13e039b293b48da0731b0338ad6e4a788a0c65
silent001 <martinnr@live.co.za>
2022-08-12 13:15:04 +0200

    Enable email verfication check

--- app/Http/Controllers/Auth/ChangePasswordController.php
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -40,7 +40,7 @@ class ChangePasswordController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth', 'verified']);
     }
 
     /**
