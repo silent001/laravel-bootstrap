39fc8e0d86b045a0d538ac7417a593798fd0178f
silent001 <martinnr@live.co.za>
2023-03-21 10:03:41 +0200

    Remove email scraping ability

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -14,7 +14,7 @@ return [
     */
 
     'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
+    'sent' => 'A password reset link has been sent to the email address provided, if it is registered in our system. Please check your email for instructions on how to reset your password',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",
